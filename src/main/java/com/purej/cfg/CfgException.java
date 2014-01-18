78aa1665a6866ebdda684f560af1546012db4930
stefan.purej <stefan.purej@8225cc81-794e-6aab-b64d-615b50e0cd88>
2014-01-18 23:30:58 +0000

    moved to google code

---
+++ src/main/java/com/purej/cfg/CfgException.java
@@ -0,0 +1,19 @@
+// Copyright (c), 2009, adopus consulting GmbH Switzerland, all rights reserved.
+package com.purej.cfg;
+
+/**
+ * This exception will be thrown if a mandatory configuration value is missing or conversion
+ * into the specified type failed.
+ *
+ * @author Stefan Mueller
+ */
+public final class CfgException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Creates a new instance of this class with a specified exception message.
+   */
+  public CfgException(String message) {
+    super(message);
+  }
+}
